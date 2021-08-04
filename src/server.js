7401545dadfe54f59068d82b3cf11dda524c410b
Anchangwan <ssdii66@gmail.com>
2021-08-04 16:55:15 +0900

    req,res 개념정리

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const PORT = 4000
 
 const app = express();
 
+const handleHome = (req, res) => res.end("this is my server");
+const handleLogin = (req, res) => res.send("Login Here");
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log(`start nodejs on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
