c842b9317f3a165d395ae666bbce91946fe0cc83
Anchangwan <ssdii66@gmail.com>
2021-08-05 17:33:23 +0900

    middleware의 기본적인 동작 및 개념

--- src/server.js
+++ src/server.js
@@ -4,10 +4,14 @@ const PORT = 4000
 
 const app = express();
 
+const gossipMiddelware = (req, res, next) =>{
+    console.log("i'm in the middleware!");
+    next();
+}
 const handleHome = (req, res) => res.end("this is my server");
 const handleLogin = (req, res) => res.send("Login Here");
 
-app.get("/", handleHome);
+app.get("/",gossipMiddelware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`start nodejs on port http://localhost:${PORT}`);
