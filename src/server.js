c5e32dbc08d5650a16e1546c021ba1c3a8cdb61e
Anchangwan <ssdii66@gmail.com>
2021-08-05 17:56:52 +0900

    app.use 개념정리

--- src/server.js
+++ src/server.js
@@ -4,15 +4,32 @@ const PORT = 4000
 
 const app = express();
 
-const gossipMiddelware = (req, res, next) =>{
-    console.log("i'm in the middleware!");
+const  logger = (req, res, next) =>{
+    console.log(`${req.method} ${req.url}`);
     next();
 }
-const handleHome = (req, res) => res.end("this is my server");
-const handleLogin = (req, res) => res.send("Login Here");
 
-app.get("/",gossipMiddelware, handleHome);
-app.get("/login", handleLogin);
+const privateMiddleware = (req, res, next) =>{
+    const url = req.url;
+    if(url === "/protect"){
+        return res.send("<h1>not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.");
+    next();
+}
+
+const handleHome = (req, res) => res.end("this is my server");
+const handleProtected = (req, res) => {
+    return res.send("this is my private lounge.");
+}
+
+
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`start nodejs on port http://localhost:${PORT}`);
 
