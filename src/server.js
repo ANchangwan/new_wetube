54882dc607d8d306a61c11e6851731c64efb8907
Anchangwan <ssdii66@gmail.com>
2021-08-07 18:44:29 +0900

    server.js 코드 정리

--- src/server.js
+++ src/server.js
@@ -1,22 +1,13 @@
 import express from "express";
 import morgan from "morgan"
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRounter from "./routers/userRouter";
 
 const PORT = 4000
 
 const app = express();
 
-const globalRouter = express.Router();
-const userRounter = express.Router();
-const videoRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-const handleEditUser = (req, res) => res.send("Edit User");
-userRounter.get("/edit", handleEditUser);
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use(morgan("dev"));
 app.use("/", globalRouter);
