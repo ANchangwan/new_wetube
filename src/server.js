b8b25bfd1b9a2391d09f22a962120801a481d837
Anchangwan <ssdii66@gmail.com>
2021-08-07 16:51:54 +0900

    router 개념정리

--- src/server.js
+++ src/server.js
@@ -5,19 +5,24 @@ const PORT = 4000
 
 const app = express();
 
+const globalRouter = express.Router();
+const userRounter = express.Router();
+const videoRouter = express.Router();
 
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
 
+const handleEditUser = (req, res) => res.send("Edit User");
+userRounter.get("/edit", handleEditUser);
 
-
-const handleHome = (req, res) => res.end("this is my server");
-const handleProtected = (req, res) => {
-    return res.send("this is my private lounge.");
-}
-
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
 
 app.use(morgan("dev"));
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.use("/", globalRouter);
+app.use("/users", userRounter);
+app.use("/videos", videoRouter);
+
 
 const handleListening = () => console.log(`start nodejs on port http://localhost:${PORT}`);
 
