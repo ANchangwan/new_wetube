d53edf2bf59a3c0ce8fb2aae7d144b1a9e51eac6
Anchangwan <ssdii66@gmail.com>
2021-08-08 16:24:15 +0900

    라우터와 컨트롤러 코드를 나눠서 코드 정리

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { edit, remove } from "../controllers/userController";
+import { edit, remove, see, logout } from "../controllers/userController";
 
 const userRounter = express.Router();
 
 
-
+userRounter.get("/logout", logout);
 userRounter.get("/edit", edit);
 userRounter.get("/remove", remove);
+userRounter.get("/:id", see);
 
 export default userRounter;
