1b98aef6635b93e34ccc6911fd276b53ce510a69
Anchangwan <ssdii66@gmail.com>
2021-08-08 18:05:00 +0900

    url변수를 숫자만 받기

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,6 +7,6 @@ const userRounter = express.Router();
 userRounter.get("/logout", logout);
 userRounter.get("/edit", edit);
 userRounter.get("/remove", remove);
-userRounter.get("/:id", see);
+userRounter.get("/:id(\\d+)", see);
 
 export default userRounter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,9 +4,9 @@ import { edit, see, deleteVideo, upload } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/delet", deleteVideo);
-videoRouter.get("/:id/edit", edit);
 videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/delet", deleteVideo);
+videoRouter.get("/:id(\\d+)/edit", edit);
 
 export default videoRouter;
