5ac927dbd8819e6d8970e65e63193fb4e8e397ae
Anchangwan <ssdii66@gmail.com>
2021-08-10 17:30:54 +0900

    mixins 적용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,16 @@ const fakeUser = {
 
 
 export const trending = (req, res) =>{
-    const videos = [1,2,3,4,5,6,7,8,9,10];
+    const videos = [{
+        title:"hello",
+    },
+    {
+        title:"Video #2",
+    },
+    {
+        title:"whatsup",
+    },
+]
     return  res.render("home", {pageTitle: "Home", fakeUser:fakeUser, videos})
 };
 export const see = (req, res) =>{
