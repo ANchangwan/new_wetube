5d833c7b2882172a62cba4dae7572e661d86c8d8
Anchangwan <ssdii66@gmail.com>
2021-08-11 21:47:34 +0900

    Array Database part one

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,24 +1,38 @@
-const fakeUser = {
-    username:"anchangwan",
-    loggedIn:false,
-};
-
-
+let videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
 export const trending = (req, res) =>{
-    const videos = [{
-        title:"hello",
-    },
-    {
-        title:"Video #2",
-    },
-    {
-        title:"whatsup",
-    },
-]
-    return  res.render("home", {pageTitle: "Home", fakeUser:fakeUser, videos})
+    
+    return  res.render("home", {pageTitle: "Home", videos})
 };
 export const see = (req, res) =>{
-    return res.render("watch");
+    
+    const {id} = req.params
+    const video = videos[id-1]
+    return res.render("watch", {pageTitle:`watching ${video.title}`});
 };
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
