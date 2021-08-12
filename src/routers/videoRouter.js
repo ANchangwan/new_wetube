087e980d32bbfb551a9fccf2b85e1daa6221b6d8
Anchangwan <ssdii66@gmail.com>
2021-08-12 19:44:41 +0900

    absolute url reletive url 차이를 이용해서 경로 설

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { edit, see, deleteVideo, upload } from "../controllers/videoController";
+import { edit, deleteVideo, upload, watch } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/delet", deleteVideo);
 videoRouter.get("/:id(\\d+)/edit", edit);
 
