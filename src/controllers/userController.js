e02fce1390e977601e297a8988fed9a51dd45de0
Anchangwan <ssdii66@gmail.com>
2021-08-20 21:05:18 +0900

    join 만들고 password hash하기(bcypt사용)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,27 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+    return res.render("join", {pageTitle:"Join"});
+}
+export const postJoin = async (req, res) => {
+    const {email, username, password, name, loacation} = req.body;
+    
+    try{
+        await User.create({
+            email,
+            username, 
+            password, 
+            name, 
+            loacation
+        });
+    }catch(error){
+        res.write("<script>alert('이미 존재하는 계정입니다.')</script>");
+        res.redirect("/join");
+    
+    }
+    console.log(req.body);
+    return res.redirect("/login");
+}
 export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("remove User");
 export const login = (req, res) => res.send("Login");
--- src/models/video.js
+++ src/models/video.js
@@ -1,7 +1,4 @@
 import mongoose  from "mongoose";
-
-
-
 const videoSchema = new mongoose.Schema({
     title: {type:String, required:true,trim:true, maxLength:80},
     description: {type:String, required:true,trim:true},
