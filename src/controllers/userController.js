090cc8537f3b2316996d6c28d11302d0eed9968b
Anchangwan <ssdii66@gmail.com>
2021-08-25 22:26:36 +0900

    github 로그인 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
-import { response } from "express";
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 
@@ -54,25 +54,86 @@ export const postLogin = async (req, res) =>{
     res.redirect("/");
 }
 
+export const startGithubLogin = (req, res) =>{
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config ={
+        client_id:process.env.GH_CLIENT,
+        allow_signup:false,
+        scope:"read:user user:email" // 공백으로 해야한다.
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl =  `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+
+export const finishGithub = async (req, res) =>{
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id:process.env.GH_CLIENT,
+        client_secret:process.env.GH_SCRET,
+        code:req.query.code
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (
+            await fetch(finalUrl, {
+            method:"POST",
+            headers:{
+                Accept: "application/json",
+            },
+        })
+    ).json();
+   
+    if ("access_token" in tokenRequest){
+        const {access_token} = tokenRequest;
+        const apiUrl = "https://api.github.com";
+        const userData = await (
+            await fetch(`${apiUrl}/user` ,{
+                headers:{
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        console.log( userData);
+        const emailData = await(
+            await fetch(`${apiUrl}/user/emails` ,{
+                headers:{
+                    Authorization: `token ${access_token}`,
+                }, 
+            })).json();
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        if(!emailObj){
+            return res.redirect("/login");
+        }
+        let user = await User.findOne({email:emailObj.email});
+        if (!user){
+            user = await User.create({
+                 name:userData.name,
+                 avatarUrl: userData.avatar_url,
+                 username:userData.login,
+                 email:emailObj.email, 
+                 password:"", 
+                 socialOnly:true,
+                 loacation:userData.location,
+                });
+           
+            }
+            req.session.loggedIn = true;
+            req.session.user = user;
+            return res.redirect("/");
+        
+    }else{
+        return res.redirect("/login")
+    }
+}
+
+export const logout = async (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
 export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("remove User");
-export const logout = async (req, res) => {
-    const session = req.session;
-    try{
-        if(session.user){
-            await session.destroy(function(error){
-                if(error){
-                    console.log(error)
-                }else{
-                    console.log("session 삭제");
-                    res.redirect("/");
-                }
-            })
-        }
-    }catch(error){
-        console.log(error);
-    }
-    res.redirect("/");
-};
 export const see = (req, res) => res.send("See User");
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, remove, see, logout } from "../controllers/userController";
+import { edit, remove, see, logout,startGithubLogin,finishGithub } from "../controllers/userController";
 
 const userRounter = express.Router();
 
@@ -7,6 +7,8 @@ const userRounter = express.Router();
 userRounter.get("/logout", logout);
 userRounter.get("/edit", edit);
 userRounter.get("/remove", remove);
+userRounter.get("/github/start", startGithubLogin);
+userRounter.get("/github/finish", finishGithub);
 userRounter.get("/:id(\\d+)", see);
 
 
