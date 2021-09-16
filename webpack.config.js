07dddccca3ab5a6f8300be13e3078fcb00e09189
Anchangwan <ssdii66@gmail.com>
2021-09-16 17:42:19 +0900

    nodemon.json 설정

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ module.exports = {
     })],
     entry: "./src/client/js/main.js",
     mode: "development",
+    watch:true,
     output:{
         filename:"js/main.js",
         path: path.resolve(__dirname, "assets"),
+        clean:true,
     },
     module: {
         
