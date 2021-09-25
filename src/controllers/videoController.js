80cb2798c2cfb7864e9209b7a4fcad2fa9827913
Anchangwan <ssdii66@gmail.com>
2021-09-25 22:57:49 +0900

    style 작업

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,8 @@ import User from "../models/User";
 
 export const home = async (req, res) =>{
   try{
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({creatAt:"desc"})
+    .populate("owner");
     return  res.render("home", {pageTitle: "Home", videos} );
   } catch{
     return res.render("server-error"); 
@@ -109,7 +110,7 @@ export const search = async (req, res) =>{
       title:{
         $regex: new RegExp(`${keyword}`,"i")
       },
-    });
+    }).populate("owner");
   }
   
   return res.render("search", {pageTitle: "Search", videos});
