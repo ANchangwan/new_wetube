07a8f60faeafbd6644f9a87390fe459171b0b184
Anchangwan <ssdii66@gmail.com>
2021-10-06 16:10:22 +0900

    videp recorder complete

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,9 +3,14 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile
 
 const handleDownload = () =>{
-
+    const a = document.createElement("a");
+    a.href = videoFile;
+    a.download = "MyRecording.webm";
+    document.body.appendChild(a);
+    a.click();
 }
 
 const handleStop = () =>{
@@ -13,30 +18,31 @@ const handleStop = () =>{
     startBtn.removeEventListener("click", handleStop);
     startBtn.addEventListener("click", handleDownload);
     recorder.stop();
-}
+};
 
 const handleStart = () =>{
     startBtn.innerText = "Stop Recording";
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
-    recorder = new MediaRecorder(strem);
+    recorder = new MediaRecorder(stream, {mimeType:"video/mp4"});
     recorder.ondataavailable = (event) =>{
-        const videoFile = URL.createObjectURL(event.data);
+        videoFile = URL.createObjectURL(event.data);
         video.srcObject = null;
         video.src = videoFile;
         video.loop = true;
         video.play();
-    }
+    };
     recorder.start();
 }
    
 
 const init = async() =>{
         stream = await navigator.mediaDevices.getUserMedia({
-            audio:true, 
+            audio:false, 
             video:true,
         });
-    video.play();
+    video.srcObject = stream;
+    video.play(stream);
 }
 
 init();
