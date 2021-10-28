9cb0fd3e4bca9d44c8cfe489dd093065a0c43b24
Anchangwan <ssdii66@gmail.com>
2021-10-29 01:51:49 +0900

    update build

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -4,8 +4,7 @@ const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 const videoId = videoContainer.dataset.id;
 const videoComments = document.querySelector(".video__comments ul");
-console.log(videoComments);
-const addComment = (text, id) =>{
+const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
   newComment.dataset.id = id;
@@ -22,51 +21,54 @@ const addComment = (text, id) =>{
   newComment.appendChild(span);
   newComment.appendChild(deletebtn);
   videoComments.prepend(newComment);
-  
-}
+};
 
-const handleSubmit = async (event) =>{
-    event.preventDefault();
-    const textarea = form.querySelector("textarea");
-    const text = textarea.value;
-    const videoId = videoContainer.dataset.id;
-    
-    if(text === ""){
-        return;
-    }
-    const response = await fetch(`/api/videos/${videoId}/comment`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text }),
-      }); 
-    if(response.status===201){
-      textarea.value = "";
-      const {newCommentId} = await response.json();
-      addComment(text, newCommentId);
-    }
-}
-const handledeleteComment = async(event) =>{
-  if(event.target.className !== "commentDeleteBtn"){
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const textarea = form.querySelector("textarea");
+  const text = textarea.value;
+  const videoId = videoContainer.dataset.id;
+
+  if (text === "") {
+    return;
+  }
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+  console.log(response, "this");
+  if (response.status === 201) {
+    textarea.value = "";
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+  }
+};
+const handledeleteComment = async (event) => {
+  
+  event.preventDefault();
+  if (event.target.className !== "commentDeleteBtn") {
     return;
   }
   const li = event.target.closest("li");
   const commentId = li.dataset.id;
 
-  const {status} = await fetch(
+  const { status } = await fetch(
     `/api/videos/${videoId}/comment/${commentId}`,
     {
-      method:"DELETE"
+      method: "DELETE",
     }
   );
-    videoComments.removeChild(li);
-    // alert("댓글을 제거할 수 없습니다.");
-  
-}
+  videoComments.removeChild(li);
+  // alert("댓글을 제거할 수 없습니다.");
+};
 
 videoComments.addEventListener("click", handledeleteComment);
 
-if(form){
-    form.addEventListener("submit", handleSubmit);
+console.log(form);
+
+if (form) {
+  form.addEventListener("submit", handleSubmit);
 }
