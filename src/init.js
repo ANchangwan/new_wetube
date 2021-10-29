43aa173b3dca49494e849e5bb207139eb8d2d2e4
Anchangwan <ssdii66@gmail.com>
2021-10-29 21:44:27 +0900

    Video 수정

--- src/init.js
+++ src/init.js
@@ -1,14 +1,14 @@
 import "regenerator-runtime";
 import "dotenv/config";
 import "./db";
-import "./models/User";
 import "./models/Video";
+import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-
 const PORT = 4000;
 
-const handleListening = () => console.log(`start nodejs on port http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`start nodejs on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
